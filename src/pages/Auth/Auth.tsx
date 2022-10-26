836aa8f681c4cc865eefd1c3f9977073d97d7163
Kamil <49162755+kamilKamilson@users.noreply.github.com>
2022-10-26 23:28:41 +0200

    Handle reditect to /auth/login route when accessing /auth

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,7 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './Auth.module.scss';
 
 export const Auth = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    location.pathname === '/auth' && navigate('/auth/login');
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.card}>
