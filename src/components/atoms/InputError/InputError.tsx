c88d81196131cc5f1ebd4cf30d212462ea54fe79
Kamil <49162755+kamilKamilson@users.noreply.github.com>
2022-10-29 12:30:27 +0200

    Fix input errors visibility

--- src/components/atoms/InputError/InputError.tsx
+++ src/components/atoms/InputError/InputError.tsx
@@ -7,7 +7,10 @@ interface Props {
 }
 
 const InputError = ({ name }: PropsWithoutRef<Props>) => {
-  const { formState: errors } = useFormContext();
+  const {
+    formState: { errors }
+  } = useFormContext();
+
   return (
     (errors as any)[name] && (
       <div className="text-xs text-red-700 flex items-center mt-1">
